4a813931edde0c940c43fdb7375466ed57a28fc2
yormanamaro <yormaneduardoamarocarrero@yormans-mbp.lan>
2024-04-11 17:49:48 -0400

    Init controler file mercadopago

---
+++ src/controllers/payment.controller.js
@@ -0,0 +1,3 @@
+// esto va a controlar los controladores de los pagos.
+
+export const createOrder =  (req, res) => res.send('Creating Order')
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { createOrder } from '../controllers/payment.controller.js';
 
 const router = Router(); // Se inicializa
 
 // Rutas asociadas a la app
-router.get('/create-order', (req, res) => res.send('Creating Order')) // Ruta para crear orden de pago
+router.get('/create-order', createOrder) // Ruta para crear orden de pago
 router.get('/success', (req, res) => res.send('success')) // Ruta para aceptar pago por el user
 router.get('/webhook', (req, res) => res.send('Webhook')); // Ruta para el estado de la transacion
 
