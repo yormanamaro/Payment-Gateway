17308fe8a262f13f2cd5d2ba2fc519c70f3026df
yormanamaro <yormaneduardoamarocarrero@yormans-mbp.lan>
2024-04-17 19:47:13 -0400

    Review error in progress

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,3 +1,26 @@
 // esto va a controlar los controladores de los pagos.
+import mercadopago from "mercadopago"; // Importamos libreria mercadopago
 
-export const createOrder =  (req, res) => res.send('Creating Order')
+
+export const createOrder = async (req, res) => {
+
+  mercadopago.configure({ // Inicializamos la libreria dando acceso con el access token del vendedor de prueba 
+    access_token: 
+    "TEST-1594407712245850-041716-1730b2f4e201c3c551721854ef5b0e07-1772220151",
+  });
+
+  const result = await mercadopago.preference.create({
+    items: [
+      {
+        title: "Laptop Lenovo",
+        unit_price: 500,
+        currency_id: "USD",
+        quantity: 1,
+      }
+    ]
+  })
+
+  console.log(result)
+  
+  res.send("creando orden")
+};
